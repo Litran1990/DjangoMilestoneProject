00ebf9d36897459512798856fd82b7c3203d84f1
Litran1990 <felipelitran@gmail.com>
2020-05-14 14:36:59 +0000

    search bar app added

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import url_reset
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
-    url(r'^register/', registration, name="registration"),
+    url(r'^register/', registration, name="register"),
     url(r'^profile/', user_profile, name="profile"),
     url(r'^password-reset/', include(url_reset))
 ]
---
+++ search/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from products.models import Product
+
+# Create your views here.
+def do_search(request):
+    products = Product.objects.filter(name__icontains=request.GET['q'])
+    return render(request, "products.html", {"products": products})
