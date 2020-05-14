d1f15f9f7afafa75bed8e7c2c4ebaefdd6e80d9f
Litran1990 <felipelitran@gmail.com>
2020-05-14 15:09:03 +0000

    font awesome and css added

--- cart/views.py
+++ cart/views.py
@@ -25,6 +25,7 @@ def adjust_cart(request, id):
     Adjust the quantity of the specified product to the specified
     amount
     """
+    print(request.POST)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
