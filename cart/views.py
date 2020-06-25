e0e6d502bdcfb68f8a3b870e0f50d62029737020
Litran1990 <felipelitran@gmail.com>
2020-06-25 10:50:54 +0000

    Order summary added to cart view

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect, reverse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def get(request):
+        try:
+            cart = request.session.get('cart', user=request.session, cart=False)
+            context = {
+                'object': cart
+            }
+            return render(request, 'cart.html', context)
+        except ObjectDoesNotExist:
+            messages.warning(request, "You do not have an active order")
+            return redirect('index')
+
 def view_cart(request):
     """A View that renders the cart contents page"""
     return render(request, "cart.html")
@@ -9,13 +21,13 @@ def view_cart(request):
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity = int(request.POST.get('quantity'))
+    # total += product.quantity * product.price
 
     cart = request.session.get('cart', {})
     if id in cart:
-        cart[id] = int(cart[id]) + quantity      
+        cart[id] = int(cart[id]) + quantity
     else:
-        cart[id] = cart.get(id, quantity) 
-
+        cart[id] = cart.get(id, quantity)
     request.session['cart'] = cart
     return redirect(reverse('index'))
 
