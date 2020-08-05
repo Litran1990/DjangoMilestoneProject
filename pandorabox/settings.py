a1b4f6327b2a183618170f795428fc83fac198ad
Litran1990 <felipelitran@gmail.com>
2020-08-05 16:58:04 +0000

    adding password reset functionality

--- pandorabox/settings.py
+++ pandorabox/settings.py
@@ -167,4 +167,12 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
